2b4706b4b94f7e1cd499c9bebd3a8d3ffd1e3d61
sonoilconte <shammersinger@yahoo.com>
2017-09-14 21:18:29 -0700

    Home component working in react

---
+++ src/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render(){
+    return(
+      <div className="container">
+
+        <div className="row">
+          <div className="col s12">
+            <h1 className="center-align">TranslateIt</h1>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col s12">
+            <div className="center-align">
+              <a href="#" className="waves-effect waves-light btn">Sign up and start translating</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col s12">
+            <div className="center-align">
+              <a href="#" className="waves-effect waves-light btn">Log in</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col s12">
+            <div className="center-align">
+              <a href="#" className="waves-effect waves-light btn">See what humans are translating</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+export default Home;
