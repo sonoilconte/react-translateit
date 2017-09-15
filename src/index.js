4878390a2e8570814ef52fa0a8a1e7e5e9314f65
sonoilconte <shammersinger@yahoo.com>
2017-09-14 21:52:59 -0700

    react router working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, browserHistory} from 'react-router';
 import Home from './Home';
+import routes from './config/routes.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(
+  <Router routes={routes} history={browserHistory}/>, document.getElementById('root')
+);
 registerServiceWorker();
